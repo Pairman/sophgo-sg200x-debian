49f5a13272e6afe55029633bb2b03454ace280c0
Null <pairmanxlr@gmail.com>
2025-11-06 00:16:04 +0800

    more mem

--- configs/licheervnano/memmap.py
+++ configs/licheervnano/memmap.py
@@ -15,7 +15,7 @@ class MemoryMap:
     # ==============
     # C906L FreeRTOS
     # ==============
-    FREERTOS_SIZE = 2 * SIZE_1M
+    FREERTOS_SIZE = 0 * SIZE_1M
     # FreeRTOS is at the end of DRAM
     FREERTOS_ADDR = DRAM_BASE + DRAM_SIZE - FREERTOS_SIZE
     FSBL_C906L_START_ADDR = FREERTOS_ADDR
@@ -35,15 +35,15 @@ class MemoryMap:
     # =========================
     # Ignore the area of FreeRTOS in u-boot and kernel
     KERNEL_MEMORY_ADDR = DRAM_BASE
-    KERNEL_MEMORY_SIZE = DRAM_SIZE - FREERTOS_SIZE
+    KERNEL_MEMORY_SIZE = DRAM_SIZE
 
     # =================
     # Multimedia buffer. Used by u-boot/kernel/FreeRTOS
     # =================
-    ION_SIZE = 63 * SIZE_1M
-    H26X_BITSTREAM_SIZE = 2 * SIZE_1M
+    ION_SIZE = 0 * SIZE_1M
+    H26X_BITSTREAM_SIZE = 0 * SIZE_1M
     H26X_ENC_BUFF_SIZE = 0
-    ISP_MEM_BASE_SIZE = 20 * SIZE_1M
+    ISP_MEM_BASE_SIZE = 0 * SIZE_1M
     FREERTOS_RESERVED_ION_SIZE = H26X_BITSTREAM_SIZE + H26X_ENC_BUFF_SIZE + ISP_MEM_BASE_SIZE
 
     # ION after FreeRTOS
@@ -58,7 +58,7 @@ class MemoryMap:
 
     # Boot logo is after the ION buffer
     # Framebuffer uses boot logo's reserved memory
-    BOOTLOGO_SIZE = 5632 * SIZE_1K
+    BOOTLOGO_SIZE = 0 * SIZE_1K
     BOOTLOGO_ADDR = ION_ADDR - BOOTLOGO_SIZE
     FRAMEBUFFER_SIZE = BOOTLOGO_SIZE
     FRAMEBUFFER_ADDR = BOOTLOGO_ADDR
