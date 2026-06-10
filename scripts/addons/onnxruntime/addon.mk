3ac239a3c782d1d216b60d083e8b49f2933d842c
scpcom <scpcom@gmx.de>
2026-06-11 00:01:04 +0200

    addons: add onnxruntime

---
+++ scripts/addons/onnxruntime/addon.mk
@@ -0,0 +1,66 @@
+SHELL = /bin/bash
+
+ifeq ($(SDK_VER), 32bit)
+CMAKE_SYSTEM_PROCESSOR = arm
+else ifeq ($(SDK_VER), 64bit)
+CMAKE_SYSTEM_PROCESSOR = aarch64
+else ifeq ($(SDK_VER), glibc_riscv64)
+CMAKE_SYSTEM_PROCESSOR = riscv64
+else ifeq ($(SDK_VER), musl_riscv64)
+CMAKE_SYSTEM_PROCESSOR = riscv64
+else
+$(error $(red)SDK_VER is invalid$(reset))
+endif
+
+# v1.22.0
+#ONNXRUNTIME_GIT_REF = f217402897f40ebba457e2421bc0a4702771968e
+# v1.22.1
+ONNXRUNTIME_GIT_REF = 89746dc19a0a1ae59ebf4b16df9acab8f99f3925
+# v1.22.2
+#ONNXRUNTIME_GIT_REF = 5630b081cd25e4eccc7516a652ff956e51676794
+
+ONNXRUNTIME_CMAKE_ENV = CMAKE_BUILD_TYPE="Release" \
+	CROSS_COMPILE=$(SDK_CROSS_COMPILE_PATH)/bin/$(SDK_CROSS_COMPILE_PREFIX) \
+	CC=$(SDK_CROSS_COMPILE_PATH)/bin/$(SDK_CROSS_COMPILE_PREFIX)gcc CFLAGS="$(SDK_TARGET_CFLAGS) -g0" \
+	CXX=$(SDK_CROSS_COMPILE_PATH)/bin/$(SDK_CROSS_COMPILE_PREFIX)g++ CXXFLAGS="$(SDK_TARGET_CXXFLAGS) -g0"
+
+#-DCMAKE_TOOLCHAIN_FILE=path/to/tool.cmake
+#-DPYTHON_EXECUTABLE=/mnt/pi/usr/bin/python3 
+#"-DPYTHON_INCLUDE_DIR=/mnt/pi/usr/include;/mnt/pi/usr/include/python3.7m" -DNUMPY_INCLUDE_DIR=/mnt/pi/folder/to/numpy/headers
+
+#-Donnxruntime_GCC_STATIC_CPP_RUNTIME=ON 
+
+ONNXRUNTIME_CMAKE_OPTS = \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=$(BUILDDIR)/onnxruntime/output \
+	-DCMAKE_SYSTEM_NAME=Linux \
+	-DCMAKE_SYSTEM_VERSION=1 \
+	-DCMAKE_SYSTEM_PROCESSOR=$(CMAKE_SYSTEM_PROCESSOR) \
+	-DCMAKE_MAKE_PROGRAM=/usr/bin/make \
+	-DCMAKE_C_COMPILER=$(SDK_CROSS_COMPILE_PATH)/bin/$(SDK_CROSS_COMPILE_PREFIX)gcc \
+	-DCMAKE_CXX_COMPILER=$(SDK_CROSS_COMPILE_PATH)/bin/$(SDK_CROSS_COMPILE_PREFIX)g++ \
+	-DONNX_CUSTOM_PROTOC_EXECUTABLE=/usr/bin/protoc \
+	-Dprotobuf_WITH_ZLIB=OFF \
+	-Donnxruntime_ENABLE_PYTHON=OFF \
+	-Donnxruntime_BUILD_SHARED_LIB=ON \
+	-Donnxruntime_BUILD_UNIT_TESTS=OFF \
+	-Donnxruntime_DEV_MODE=OFF
+
+$(BUILDDIR)/onnxruntime-prepare-stamp:
+	@mkdir -p $(BUILDDIR)/onnxruntime/
+	@cd $(BUILDDIR)/onnxruntime/ && git clone -b main $(GIT_USER_URL)/onnxruntime
+	@cd $(BUILDDIR)/onnxruntime/onnxruntime/ && git checkout $(ONNXRUNTIME_GIT_REF)
+	@touch $@
+
+$(BUILDDIR)/onnxruntime-protoc-stamp:
+	@apt-get install -y protobuf-compiler
+	@touch $@
+
+$(BUILDDIR)/onnxruntime-stamp: $(BUILDDIR)/onnxruntime-prepare-stamp $(BUILDDIR)/onnxruntime-protoc-stamp
+	@mkdir -p $(SDK_OSS_TARBALL_DIR)
+	@mkdir -p $(BUILDDIR)/onnxruntime/build
+	@mkdir -p $(BUILDDIR)/onnxruntime/output
+	@cd $(BUILDDIR)/onnxruntime/build ; $(ONNXRUNTIME_CMAKE_ENV) cmake $(BUILDDIR)/onnxruntime/onnxruntime/cmake -DCMAKE_INSTALL_PREFIX=$(BUILDDIR)/onnxruntime/output $(ONNXRUNTIME_CMAKE_OPTS)
+	@cd $(BUILDDIR)/onnxruntime/build ; $(ONNXRUNTIME_CMAKE_ENV) make install
+	@tar -C $(BUILDDIR)/onnxruntime/output -czf $(SDK_OSS_TARBALL_DIR)/onnxruntime.tar.gz include lib
+	@touch $@
